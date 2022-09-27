3759972c6db4bfad46a6d0f47816aefe5fbc7212
akash-k7 <92331024+akash-k7@users.noreply.github.com>
2022-09-27 17:24:12 +0530

    Update funcs.sol

--- funcs.sol
+++ funcs.sol
@@ -2,27 +2,29 @@
 pragma solidity ^0.8.1;
 
 contract Storage {
-
-    uint256 number;
+    uint256 square;
+    uint256 total;
+    uint256 product;
     
-    // function to store value to number variable
-    function store(uint256 num) public {
-        number = num;
+    // a function to find square of a number
+    // which has one output parameter
+    function findSquare(uint256 number1) private pure returns (uint256 _total) {
+        return number1 * number1;
     }
     
-    // calling store function from another function
-    function callStore() public {
-        store(1001);
+    // a function to find the total and Product of two numbers
+    // has two output parameters
+    function findTotalandProduct(uint256 number1, uint256 number2) private pure returns (uint256 _total, uint256 _product) {
+        return (number1 + number2, number1 * number2);
     }
     
-    // calling store function from another function
-    // by specifying input parameter name
-    function callStoreAnother() public {
-        store({num: 1001});
-    }
-
-    // function to retrieve the data in number variable
-    function retrieve() public view returns (uint256){
-        return number;
+    // the function that calls and stores the data
+    function storeValues(uint256 number1, uint256 number2) public {
+        
+        // storing the result of function findSquare() into variable
+        square = findSquare(number1);
+        
+        // storing the result of function findTotalandProduct() into a tuple
+        (total, product) = findTotalandProduct(number1, number2);
     }
 }
